f1e446f84896fb24bb754426e2728339145f197b
FrancieleCSantos <francielecoceicaodossantos93@gmail.com>
2024-08-31 12:08:18 -0300

    Testes realizados - Cadastrar uma transação, Cadastrar uma Saída e Excluir uma transação

--- cypress/e2e/finaces.cy.js
+++ cypress/e2e/finaces.cy.js
@@ -1,2 +1,44 @@
 /// <reference types="cypress" />
 
+describe('Transações', () => {
+
+    beforeEach(() => {
+        cy.visit("https://devfinance-agilizei.netlify.app/")
+    })
+
+    it('Cadastrar uma entrada', () => {
+
+        criarTransacao("Freela", 250)
+        cy.get("tbody tr td.description")
+            .should("have.text", "Freela")
+    });
+
+    it('Cadastrar uma saída', () => {
+
+        criarTransacao("Cinema", -52)
+        cy.get("tbody tr td.description")
+            .should("have.text", "Cinema")
+    });
+
+    it('Exluir uma transação', () => {
+        criarTransacao("Freela", 100)
+        criarTransacao("Mesada", 100)
+
+        cy.contains(".description", "Freela")
+            .parent()
+            .find('img')
+            .click()
+
+        cy.get('tbody tr').should("have.length", 1)
+
+    });
+})
+
+function criarTransacao(descricao, valor) {
+    cy.contains("Nova Transação").click()
+    cy.get('#description').type(descricao)
+    cy.get('#amount').type(valor)
+    cy.get('#date').type("2024-02-15")
+
+    cy.contains('button', 'Salvar').click()
+}
